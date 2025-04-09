fbeedb8ab32559001fd8bf249630c0f5ca9682cb
Chad Phillips <chad@apartmentlines.com>
2025-04-08 22:20:02 -0400

    add post_request() to utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,18 @@
 import argparse
 import sys
 import pytest
-from unittest.mock import patch
+import requests
+import tenacity
+from unittest.mock import patch, Mock
 from transcription_pipeline_manager.utils import (
     positive_int,
     fail_hard,
+    post_request,
+)
+
+from transcription_pipeline_manager.constants import (
+    DEFAULT_RETRY_ATTEMPTS,
+    DOWNLOAD_TIMEOUT,
 )
 
 
@@ -21,3 +29,51 @@ def test_fail_hard(caplog):
         fail_hard("Test error")
         mock_exit.assert_called_with(1)
         assert "Test error" in caplog.text
+
+
+@patch("requests.post")
+def test_post_request_success(mock_post):
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_post.return_value = mock_response
+    url = "http://example.com"
+    data = {"key": "value"}
+    response = post_request(url, data=data)
+    assert response.status_code == 200
+    mock_post.assert_called_once_with(url, data=data, timeout=DOWNLOAD_TIMEOUT)
+
+
+@patch("requests.post")
+@patch("tenacity.nap.time.sleep")
+def test_post_request_failure(_, mock_post):
+    mock_post.side_effect = requests.exceptions.RequestException
+    url = "http://example.com"
+    data = {"key": "value"}
+    with pytest.raises(tenacity.RetryError):
+        post_request(url, data=data)
+    mock_post.assert_called_with(url, data=data, timeout=DOWNLOAD_TIMEOUT)
+    assert mock_post.call_count == DEFAULT_RETRY_ATTEMPTS
+
+
+@patch("requests.post")
+def test_post_request_json_success(mock_post):
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_post.return_value = mock_response
+    url = "http://example.com"
+    data = {"key": "value"}
+    response = post_request(url, data=data, json=True)
+    assert response.status_code == 200
+    mock_post.assert_called_once_with(url, json=data, timeout=DOWNLOAD_TIMEOUT)
+
+
+@patch("requests.post")
+@patch("tenacity.nap.time.sleep")
+def test_post_request_json_failure(_, mock_post):
+    mock_post.side_effect = requests.exceptions.RequestException
+    url = "http://example.com"
+    data = {"key": "value"}
+    with pytest.raises(tenacity.RetryError):
+        post_request(url, data=data, json=True)
+    mock_post.assert_called_with(url, json=data, timeout=DOWNLOAD_TIMEOUT)
+    assert mock_post.call_count == DEFAULT_RETRY_ATTEMPTS
--- transcription_pipeline_manager/utils.py
+++ transcription_pipeline_manager/utils.py
@@ -1,6 +1,14 @@
 import sys
 import argparse
 import logging
+import requests
+from typing import Any
+from tenacity import retry, stop_after_attempt, wait_exponential
+from .constants import (
+    DEFAULT_RETRY_ATTEMPTS,
+    DEFAULT_RETRY_BACKOFF,
+    DOWNLOAD_TIMEOUT,
+)
 
 def positive_int(value: str) -> int:
     ivalue = int(value)
@@ -13,3 +21,20 @@ def fail_hard(message: str) -> None:
     logger = logging.getLogger(__name__)
     logger.error(message)
     sys.exit(1)
+
+
+@retry(
+    stop=stop_after_attempt(DEFAULT_RETRY_ATTEMPTS),
+    wait=wait_exponential(multiplier=DEFAULT_RETRY_BACKOFF),
+)
+def post_request(url: str, data: dict[str, Any], json: bool = False) -> requests.Response:
+    kwargs: dict[str, Any] = {
+        "timeout": DOWNLOAD_TIMEOUT,
+    }
+    if json:
+        kwargs["json"] = data
+    else:
+        kwargs["data"] = data
+    response = requests.post(url, **kwargs)
+    response.raise_for_status()
+    return response
